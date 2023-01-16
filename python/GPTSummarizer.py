7ef5847b0eecfb73c9de2ce43613632d9219e405
Ian Cooperman <ian.pl.cooperman@gmail.com>
2023-01-16 14:31:42 -0800

    The sumamrizer is working

--- python/GPTSummarizer.py
+++ python/GPTSummarizer.py
@@ -11,9 +11,8 @@ class GPTSummmarizer:
         
     def summarize(self, text: str) -> str:
         prompt = self.command + text
-        completion = openai.Completion.create(engine=self.engine, prompt=prompt)
-        return completion.choices[0].text
-
+        completion = openai.Completion.create(engine=self.engine, prompt=prompt, max_tokens=128)
+        return completion.choices[0].text # type: ignore
 
 
 
@@ -24,12 +23,8 @@ if __name__ == "__main__":
     spokesperson for the Walker County Coroner’s office in Alabama confirmed with People that
      Harris was taken to the hospital, but didn’t make it. He was 31 years old. Harris auditioned for the 13th season of “American Idol” by singing the Allman Brothers’ “Soulshine.” “It’s like you sing because you have to sing, not because you want to sing, and I mean that in the deepest way,” said judge Keith Urban. “And that’s why it’s so believable and real, and it’s greater than the sum of its parts.” Harris broke into tears after receiving a unanimous vote from all three judges to advance him to the next round. He finished in sixth place. Harris died in his hometown of Jasper, Alabama. In a 2014 interview with Nexstar’s KSWB, he spoke of how his hometown led him to a passion for music. “You can live in Las Vegas or Hollywood and it can be a distraction,” said Harris.  “Being from a small town there’s not much to do but play music.”
     """
-
-    home_folder = os.path.expanduser('~')
-
-
-
-    summarizer = GPTSummmarizer(os.environ["OPENAI_API_KEY"])
-    print(os.environ)
-    # summary = summarizer.summarize(test_text)
+    settings = json.load(open("../settings.json", 'r', encoding='utf-8'))
+    summarizer = GPTSummmarizer(settings["OPENAI_API_KEY"])
+    summary = summarizer.summarize(test_text)
+    print(summary)
 
