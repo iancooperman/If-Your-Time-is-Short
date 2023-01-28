4b653356f0ff0a9c7ff8fb0a5f759b8223a1d8ee
Ian Cooperman <ian.pl.cooperman@gmail.com>
2023-01-28 15:21:17 -0800

    Wrote basic structure of mainloop in prep for final jigsaw solve

---
+++ python/ArticleHandler.py
@@ -0,0 +1,79 @@
+import abc
+import re
+import urllib.parse
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# ALL HANDLERS MUST BE PLACED IN THIS FILE OR THEY WILL BE IGNORED BY Aritcle.py ( specifically Article._choose_handler() )!!!
+
+
+# Abstract Base Class
+class ArticleHandler(metaclass=abc.ABCMeta):
+    def __init__(self, url) -> None:
+        self._url = url
+        self._build_soup(self._url)
+        
+
+    def _build_soup(self, url) -> BeautifulSoup:
+        response = requests.get(url)
+        if response.status_code == 200:
+            self._content = response.text
+        else:
+            requests.exceptions.HTTPError(response.status_code)
+
+        self._soup = BeautifulSoup(self._content, 'html.parser')
+
+    @classmethod
+    @abc.abstractmethod
+    def _valid_url(self, url) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_title(self) -> str:
+        pass
+    
+    @abc.abstractmethod
+    def get_body(self) -> str:
+        pass
+
+    def __repr__(self):
+        return str(self._soup)
+
+class ReutersArticleHandler(ArticleHandler):
+
+    # five popular domains on /r/news
+    #   abcnews.go.com
+    #   apnews.com
+    #   reuters.com
+    #   cnn.com
+    #   nbcnews.com
+
+    @classmethod
+    def _valid_url(cls, url) -> bool:
+
+        parsed = urllib.parse.urlparse(url)
+
+        # not great, but a lot more readable than trying to do everything with a single regular expression
+        return parsed.scheme in ['https', 'http'] and parsed.netloc == "www.reuters.com" and any([
+            re.match(r"/world/[a-z]+/.+", parsed.path),
+            re.match(r"/legal/.+", parsed.path),
+            re.match(r"/business/[a-z]+/.+", parsed.path),
+            re.match(r"/technology/.+", parsed.path),
+            re.match(r"/lifestyle/.+", parsed.path),
+        ]) 
+
+    def get_title(self):
+        h1 = self._soup.select_one('h1[data-testid="Heading"]')
+        title = h1.get_text()
+        return title
+
+    def get_body(self):
+        paragraphs = []
+        for p in self._soup.select('p[data-testid^="paragraph-"]'):
+            paragraphs.append(p.get_text())
+        return " ".join(paragraphs)
+    
+    def __repr__(self):
+        return str(self._soup)
--- python/main.py
+++ python/main.py
@@ -1,3 +1,32 @@
-from Settings import SETTINGS
+import logging
+import sys
+import time
 
-print(SETTINGS.OPENAI_API_KEY)
+from Settings import Settings
+
+
+def wait_one_hour():
+    time.sleep(3600)
+
+def main():
+    logging.basicConfig(filename='iytis.log', encoding='utf-8', level=logging.DEBUG, format='%(levelname)s [%(asctime)s]: %(message)s (%(filename)s:%(lineno)s)')
+
+    logger = logging.getLogger()
+
+
+    try:
+        while True:
+            logger.info("get top 3 posts from Rising in /r/news")
+            logger.info("summarize each of them")
+            logger.info("return each summary as a comment on their respective post")
+            
+            logger.info("wait for one hour")
+            wait_one_hour()
+            
+    except Exception as e:
+        logging.error(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
