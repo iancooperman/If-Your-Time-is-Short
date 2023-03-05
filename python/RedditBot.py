ec6b877b24998827a13e49a23804061452a6091e
Ian Cooperman <ian.pl.cooperman@gmail.com>
2023-03-04 21:35:18 -0800

    Retrieving links from two subreddits

--- python/RedditBot.py
+++ python/RedditBot.py
@@ -10,16 +10,30 @@ settings.load_settings("settings.json")
 class RedditBot:
     def __init__(self) -> None:
         self._reddit = Reddit(
-            client_id="my client id", 
+            client_id=settings["reddit"]["client_id"], 
             client_secret=settings["reddit"]["secret"], 
             user_agent="my user agent", 
             username=settings["reddit"]["username"], 
-            password=settings["reddit"]["password"])
+            password=settings["reddit"]["password"]
+        )
 
         assert not self._reddit.read_only
+
+        self._subreddits = [self._reddit.subreddit(sub) for sub in settings["reddit"]["subreddits"]]
+
+
+    def get_articles(self) -> list[str]:
+        urls = []
+        for sub in self._subreddits:
+            for post in sub.rising(limit=3): # get the top 3 posts at the time 
+                urls.append(post.url)
+
+        return urls
 
 
 if __name__ == "__main__":
     reddit_bot = RedditBot()
+    urls = reddit_bot.get_articles()
+    print(urls)
 
 
--- python/main.py
+++ python/main.py
@@ -1,3 +1,36 @@
-from Settings import SETTINGS
+import logging
 
-print(SETTINGS.OPENAI_API_KEY)
+from praw import Reddit
+from Settings import Settings
+
+
+def main():
+    # retrieve settings
+    settings = Settings()
+    settings.load_settings("settings.json")
+
+    # initialize Reddit instance
+    reddit = Reddit(
+        client_id="my client id", 
+        client_secret=settings["reddit"]["secret"], 
+        user_agent="IfYourTimeIsShort v0.1 by /u/BananaZen314159"
+        username=settings["reddit"]["username"], 
+        password=settings["reddit"]["password"]
+    )
+
+    subreddits = settings["reddit"]["subreddits"]
+
+    urls = []
+    for sub_name in subreddits:
+        subreddit = reddit.subreddit(sub_name)
+        print(subreddit)
+        for post in subreddit.rising(limit=3): # get the top 3 posts at the time 
+            print(post.title)
+
+            # urls.append(post.url)
+    
+    print(urls)
+
+
+if __name__ == "__main__":
+    main()
