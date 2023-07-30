0a5831930aa975a16dce385e5660a513e88f6ac1
Ian Cooperman <ian.pl.cooperman@gmail.com>
2023-07-29 20:24:27 -0700

    Switched to model gpt-3.5-turbo

--- python/GPTSummarizer.py
+++ python/GPTSummarizer.py
@@ -10,17 +10,27 @@ import openai
 
 
 class GPTSummarizer:
-    def __init__(self, openai_api_key: str, engine: str="text-davinci-003", **kwargs: dict[str, str]):
+    def __init__(self, openai_api_key: str, model: str="gpt-3.5-turbo", **kwargs: dict[str, str]):
         openai.api_key = openai_api_key
-        self.engine = engine
+        self.model = model
         self.params = kwargs
 
-        self.command = "Please summarize the following text in exactly 3 sentences, leaving no whitespace above or below the paragraph created: \n"
+        self.prompt = 'You will be given the complete text of an online news article. Please summarize the article in exactly 3 bullet points, one single, complete, non-run-on sentence per bullet. Output in markdown.'
+        # example text sourced from https://www.politifact.com/factchecks/2023/jul/24/kamala-harris/do-Florida-school-standards-say-enslaved-people/
+        self.example = '- The Florida Board of Education set new social studies standards for middle schoolers July 19.\n- In a section about the duties and trades performed by enslaved people, the state adopted a clarification that said "instruction includes how slaves developed skills which, in some instances, could be applied for their personal benefit.\n- Experts on Black history said that such language is factually misleading and offensive."'
         
     def summarize(self, text: str) -> str:
-        prompt = self.command + text
-        completion = openai.Completion.create(engine=self.engine, prompt=prompt, max_tokens=128)
-        return completion.choices[0].text # type: ignore
+        prompt = self.prompt + text
+        completion = openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": self.prompt},
+                {"role": "assistant", "content": self.example},
+                {"role": "user", "content": text}
+            ]
+        )
+        logging.debug(completion)
+        return completion.choices[0]["message"]["content"] # type: ignore
     
     def url_to_summary(self, url: str) -> str | None:
         # parse the url and ensure that the site's robots.txt allows crawling on the url
@@ -58,6 +68,6 @@ if __name__ == "__main__":
     config.read("./" + "config.ini")
 
     summarizer = GPTSummarizer(config.get("openai", "api_key"))
-    summary = summarizer.url_to_summary("https://www.npr.org/2023/07/29/1190701793/economy-fed-earnings-barbie-taylor-swift")
+    summary = summarizer.url_to_summary("https://www.ctinsider.com/columnist/article/house-built-on-fairfield-lot-18261063.php")
     print(summary)
 
