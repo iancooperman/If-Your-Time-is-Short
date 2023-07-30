5ba9d3aadae9028edac470ade9165bd518ded711
Ian Cooperman <ian.pl.cooperman@gmail.com>
2023-07-30 15:56:48 -0700

    Cleaned up summarizer code a bit

--- python/GPTSummarizer.py
+++ python/GPTSummarizer.py
@@ -1,13 +1,13 @@
 import configparser
-from pathlib import Path
-from urllib.robotparser import RobotFileParser
-from urllib.parse import urlparse
-from newspaper import Article
-from newspaper.article import ArticleException
 import logging
-import configparser
+from pathlib import Path
+from urllib.parse import urlparse
+from urllib.robotparser import RobotFileParser
 
 import openai
+from newspaper import Article
+from newspaper.article import ArticleException
+from util import *
 
 
 class GPTSummarizer:
@@ -21,7 +21,6 @@ class GPTSummarizer:
         self.example = '- The Florida Board of Education set new social studies standards for middle schoolers July 19.\n- In a section about the duties and trades performed by enslaved people, the state adopted a clarification that said "instruction includes how slaves developed skills which, in some instances, could be applied for their personal benefit.\n- Experts on Black history said that such language is factually misleading and offensive."'
         
     def summarize(self, text: str) -> str:
-        prompt = self.prompt + text
         response = openai.ChatCompletion.create(
             model=self.model,
             messages=[
@@ -33,16 +32,18 @@ class GPTSummarizer:
         logging.debug(response)
         return response["choices"][0]["message"]["content"] # type: ignore
     
+    # Given a url, provide a 3-bulleted summary of the news article the url directs to
     def url_to_summary(self, url: str) -> str | None:
         # parse the url and ensure that the site's robots.txt allows crawling on the url
         try:
             domain: str = urlparse(url).netloc
             scheme: str = urlparse(url).scheme
             robots_txt_url: str = f"{scheme}://{domain}/robots.txt"
-            robot_file_parser: RobotFileParser = RobotFileParser(robots_txt_url)
+            robot_file_parser: RobotFileParser = RobotFileParser()
+            robot_file_parser.set_url(robots_txt_url)
             robot_file_parser.read()
 
-            if robot_file_parser.can_fetch("/u/IfYourTimeIsShort", url): # if crawling is allowed...
+            if robot_file_parser.can_fetch("*", url): # if crawling is allowed...
                 logging.debug(f"{robots_txt_url} allows parsing of {url}")
                 
                 # retrieve the text content of the article
@@ -53,7 +54,7 @@ class GPTSummarizer:
                 article_body: str = article.text
                 
                 # generate a summary from the extracted article text
-                generated_summary: str = summarizer.summarize(article_body)
+                generated_summary: str = self.summarize(article_body)
                 return generated_summary
             else:
                 logging.debug(f"{robots_txt_url} prohibits parsing of {url}")
@@ -69,7 +70,9 @@ if __name__ == "__main__":
     config: configparser.ConfigParser = configparser.ConfigParser(allow_no_value=True)
     config.read("./" + "config.ini")
 
+    logging_config()
+
     summarizer = GPTSummarizer(config.get("openai", "api_key"))
-    summary = summarizer.url_to_summary("https://www.iflscience.com/the-worlds-largest-wind-turbine-has-been-switched-on-70047")
+    summary = summarizer.url_to_summary("https://www.washingtonpost.com/national-security/2023/07/22/air-force-general-ai-judeochristian/")
     print(summary)
 
