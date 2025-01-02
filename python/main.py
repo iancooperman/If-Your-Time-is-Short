ff4bbfe783c727983fe83c20dcb09d85417209db
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2025-01-01 16:27:54 -0800

    Made it possible to load any config file (via command line argument), fixed bug where bot wouldn't monitor all configured subreddits, and refactored submission sort parsing to eliminate injection opportunity

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import configparser
 import logging
 import sqlite3
 import time
+import sys
 
 import praw
 from GPTSummarizer import GPTSummarizer
@@ -9,11 +10,15 @@ from praw import Reddit
 from util import *
 
 # globals
-config_file_name: str = "config.ini"
-config: configparser.ConfigParser = configparser.ConfigParser(allow_no_value=True)
-config.read(config_file_name)
-logging.info(f"{config_file_name} loaded")
-summarizer = GPTSummarizer(config.get("openai", "API_Key"))
+config: configparser.ConfigParser = None
+summarizer: GPTSummarizer = None
+
+# utility function for loading the settings from a config file
+def load_config(config_file_name: str = "config.ini"): # default file name is 'config.ini'
+    global config
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(config_file_name)
+    logging.info(f"{config_file_name} loaded")
 
 
 def comment_format(raw_summary: str) -> str:
@@ -49,8 +54,23 @@ def db_init() -> sqlite3.Connection:
     return connection
 
 def main() -> None:
+    # parse command line arguments
+    argv: list[str] = sys.argv
+
+    # load desired config file
+    try:
+        config_file_name: str = argv[1]
+    except IndexError:
+        config_file_name = "config.ini" # default filename of 'config.ini'
+
+    load_config(config_file_name)
+
+
     # initialize logger
     logging_config()
+
+    global summarizer
+    summarizer = GPTSummarizer(config.get("openai", "API_Key"))
 
     reddit: Reddit = reddit_init()
     logging.info('Reddit instance initialized')
@@ -59,6 +79,7 @@ def main() -> None:
     logging.info("DB connection established")
 
     subreddit_names: list[str] = config.get("reddit.submissions", "subreddits").split(",")
+    logging.info(f"Subreddits to monitor: {', '.join(subreddit_names)}")
 
     # time in seconds before refreshing the posts
     subreddit_refresh_delay: float = float(config.get("reddit.submissions", "refresh_delay"))
@@ -66,43 +87,58 @@ def main() -> None:
     # mainloop
     done: bool = False
     while not done:
-        submissiion_urls: list[str] = []
+        submission_urls: list[str] = []
         for subreddit_name in subreddit_names:
+            logging.info(f"Scanning /r/{subreddit_name}")
             subreddit: praw.models.SubredditHelper = reddit.subreddit(subreddit_name) # type: ignore
 
-            submission_sort: str = config.get("reddit.submissions", "post_sort")
+            submission_sort: str = config.get("reddit.submissions", "post_sort").lower()
             submission_limit: int = config.getint("reddit.submissions", "post_limit")
 
-            generated_code_for_submission_generator_creation: str = f"subreddit.{submission_sort}(limit={submission_limit})"
-            logging.debug(f"Generated code: `{generated_code_for_submission_generator_creation}`")
+            # generated_code_for_submission_generator_creation: str = f"subreddit.{submission_sort}(limit={submission_limit})"
+            # logging.debug(f"Generated code: `{generated_code_for_submission_generator_creation}`")
 
-            # run generated code
-            # TODO: using eval is a terrible idea
-            submissions: list[praw.models.Submission] = list(eval(generated_code_for_submission_generator_creation)) # get the top n posts at the time according the given sort
+            # # run generated code
+            # # TODO: using eval is a terrible idea
+            # submissions: list[praw.models.Submission] = list(eval(generated_code_for_submission_generator_creation)) # get the top n posts at the time according the given sort
+
+            submissions: list[praw.models.Submission] = None
+            match submission_sort:
+                case "hot":
+                    submissions = list(subreddit.hot(limit=submission_limit))
+                case "new":
+                    submissions = list(subreddit.new(limit=submission_limit))
+                case "rising":
+                    submissions = list(subreddit.rising(limit=submission_limit))
+                case "top":
+                    submissions = list(subreddit.top(limit=submission_limit))
+                case _:
+                    logging.error(f"Invalid submission sort option: {submission_sort}. Terminating.")
+                    quit(1)
+            logging.info(f"Sorting by {submission_sort}")
         
-        for submission in submissions:
-            db_cursor = db_conn.cursor()
-            db_cursor.execute(f"SELECT * FROM submissions WHERE id = '{submission.id}'")
-            submission_already_seen: bool = db_cursor.fetchone()
-            if submission_already_seen:
-                continue
-            else:
-                db_cursor.execute(f"INSERT INTO submissions VALUES ('{submission.id}')")
-                db_conn.commit()
-            
-            
-            # parse the submission's url and ensure that the site's robots.txt allows crawling on the url
-            submission_url: str = submission.url
-            generated_submission_summary: str = summarizer.url_to_summary(submission_url) #type: ignore
+            for submission in submissions:
+                db_cursor = db_conn.cursor()
+                db_cursor.execute(f"SELECT * FROM submissions WHERE id = '{submission.id}'")
+                submission_already_seen: bool = db_cursor.fetchone()
+                if submission_already_seen:
+                    continue
+                else:
+                    db_cursor.execute(f"INSERT INTO submissions VALUES ('{submission.id}')")
+                    db_conn.commit()
+                
+                # parse the submission's url and ensure that the site's robots.txt allows crawling on the url
+                submission_url: str = submission.url
+                generated_submission_summary: str = summarizer.url_to_summary(submission_url) #type: ignore
 
-            # format the generated summary into something more visually appealing
-            if generated_submission_summary:
-                formatted_submission_summary: str = comment_format(generated_submission_summary)
-            
-                logging.info(f"Summary:\n{formatted_submission_summary}")
+                # format the generated summary into something more visually appealing
+                if generated_submission_summary:
+                    formatted_submission_summary: str = comment_format(generated_submission_summary)
+                
+                    logging.info(f"Summary:\n{formatted_submission_summary}")
 
-                # submit the comment!
-                submission.reply(formatted_submission_summary)
+                    # submit the comment!
+                    submission.reply(formatted_submission_summary)
 
         logging.info(f"Halting for {subreddit_refresh_delay} {'seconds' if subreddit_refresh_delay != 1 else 'second'}.")
         time.sleep(subreddit_refresh_delay)
