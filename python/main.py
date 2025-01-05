5ec21632f799b90b86d703fa1cc231a6f619c378
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2025-01-05 12:11:03 -0800

    Removed loop. Bot logic now only runs once per script execution. Loop to be replaced with cron job in near future.

--- python/main.py
+++ python/main.py
@@ -86,66 +86,62 @@ def main() -> None:
     # time in seconds before refreshing the posts
     subreddit_refresh_delay: float = float(config.get("reddit.submissions", "refresh_delay"))
 
-    # mainloop
-    done: bool = False
-    while not done:
-        submission_urls: list[str] = []
-        for subreddit_name in subreddit_names:
-            logging.info(f"Scanning /r/{subreddit_name}")
-            subreddit: praw.models.SubredditHelper = reddit.subreddit(subreddit_name) # type: ignore
+    # loop through subreddits and posts up to post n
+    submission_urls: list[str] = []
+    for subreddit_name in subreddit_names:
+        logging.info(f"Scanning /r/{subreddit_name}")
+        subreddit: praw.models.SubredditHelper = reddit.subreddit(subreddit_name) # type: ignore
 
-            submission_sort: str = config.get("reddit.submissions", "post_sort").lower()
-            submission_limit: int = config.getint("reddit.submissions", "post_limit")
+        submission_sort: str = config.get("reddit.submissions", "post_sort").lower()
+        submission_limit: int = config.getint("reddit.submissions", "post_limit")
 
-            # generated_code_for_submission_generator_creation: str = f"subreddit.{submission_sort}(limit={submission_limit})"
-            # logging.debug(f"Generated code: `{generated_code_for_submission_generator_creation}`")
+        # generated_code_for_submission_generator_creation: str = f"subreddit.{submission_sort}(limit={submission_limit})"
+        # logging.debug(f"Generated code: `{generated_code_for_submission_generator_creation}`")
 
-            # # run generated code
-            # # TODO: using eval is a terrible idea
-            # submissions: list[praw.models.Submission] = list(eval(generated_code_for_submission_generator_creation)) # get the top n posts at the time according the given sort
+        # # run generated code
+        # # TODO: using eval is a terrible idea
+        # submissions: list[praw.models.Submission] = list(eval(generated_code_for_submission_generator_creation)) # get the top n posts at the time according the given sort
 
-            submissions: list[praw.models.Submission] = None
-            match submission_sort:
-                case "hot":
-                    submissions = list(subreddit.hot(limit=submission_limit))
-                case "new":
-                    submissions = list(subreddit.new(limit=submission_limit))
-                case "rising":
-                    submissions = list(subreddit.rising(limit=submission_limit))
-                case "top":
-                    submissions = list(subreddit.top(limit=submission_limit))
-                case _:
-                    logging.error(f"Invalid submission sort option: {submission_sort}. Terminating.")
-                    quit(1)
-            logging.info(f"Sorting by {submission_sort}")
-        
-            for submission in submissions:
-                # db_cursor = db_conn.cursor()
-                # db_cursor.execute(f"SELECT * FROM submissions WHERE id = '{submission.id}'")
-                # submission_already_seen: bool = db_cursor.fetchone()
-                submission_already_seen: bool = iytis_db.submission_present(submission.id)
-                if submission_already_seen:
-                    continue
-                else:
-                    # db_cursor.execute(f"INSERT INTO submissions VALUES ('{submission.id}')")
-                    # db_conn.commit()
-                    iytis_db.insert_submission(submission.id)
-                
-                # parse the submission's url and ensure that the site's robots.txt allows crawling on the url
-                submission_url: str = submission.url
-                generated_submission_summary: str = summarizer.url_to_summary(submission_url) #type: ignore
+        submissions: list[praw.models.Submission] = None
+        match submission_sort:
+            case "hot":
+                submissions = list(subreddit.hot(limit=submission_limit))
+            case "new":
+                submissions = list(subreddit.new(limit=submission_limit))
+            case "rising":
+                submissions = list(subreddit.rising(limit=submission_limit))
+            case "top":
+                submissions = list(subreddit.top(limit=submission_limit))
+            case _:
+                logging.error(f"Invalid submission sort option: {submission_sort}. Terminating.")
+                quit(1)
+        logging.info(f"Sorting by {submission_sort}")
+    
+        for submission in submissions:
+            # db_cursor = db_conn.cursor()
+            # db_cursor.execute(f"SELECT * FROM submissions WHERE id = '{submission.id}'")
+            # submission_already_seen: bool = db_cursor.fetchone()
+            submission_already_seen: bool = iytis_db.submission_present(submission.id)
+            if submission_already_seen:
+                continue
+            else:
+                # db_cursor.execute(f"INSERT INTO submissions VALUES ('{submission.id}')")
+                # db_conn.commit()
+                iytis_db.insert_submission(submission.id)
+            
+            # parse the submission's url and ensure that the site's robots.txt allows crawling on the url
+            submission_url: str = submission.url
+            generated_submission_summary: str = summarizer.url_to_summary(submission_url) #type: ignore
 
-                # format the generated summary into something more visually appealing
-                if generated_submission_summary:
-                    formatted_submission_summary: str = comment_format(generated_submission_summary)
-                
-                    logging.info(f"Summary:\n{formatted_submission_summary}")
+            # format the generated summary into something more visually appealing
+            if generated_submission_summary:
+                formatted_submission_summary: str = comment_format(generated_submission_summary)
+            
+                logging.info(f"Summary:\n{formatted_submission_summary}")
 
-                    # submit the comment!
-                    submission.reply(formatted_submission_summary)
+                # submit the comment!
+                submission.reply(formatted_submission_summary)
 
-        logging.info(f"Halting for {subreddit_refresh_delay} {'seconds' if subreddit_refresh_delay != 1 else 'second'}.")
-        time.sleep(subreddit_refresh_delay)
 
 if __name__ == "__main__":
     main()
